6f217904c3a11de625277cd0e99bac10ddf122d5
louie1966 <renedeleeuw66@gmail.com>
2017-08-18 00:17:54 +0200

    Property & Event Binding Code Changes

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -12,15 +12,18 @@ export class JokeListComponent implements OnInit {
     this.jokes = [
       {
         setup: "What did the cheese say when it looked in the mirror?",
-        punchline: "Hello-Me (Halloumi)"
+        punchline: "Hello-Me (Halloumi)",
+        hide: true
       },
       {
         setup: "What kind of cheese do you use to disguise a small horse?",
-        punchline: "Mask-a-pony (Mascarpone)"
+        punchline: "Mask-a-pony (Mascarpone)",
+        hide: true
       },
       {
         setup: "A kid threw a lump of cheddar at me",
-        punchline: "I thought ‘That’s not very mature’"
+        punchline: "I thought ‘That’s not very mature’",
+        hide: true
       },
     ];
   }
